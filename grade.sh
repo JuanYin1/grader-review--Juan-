b618aca1f5caf858df70892bf11d2d7ed636cbfd
JuanYin1 <79886525+JuanYin1@users.noreply.github.com>
2023-02-23 19:56:32 -0800

    changeshfile

--- grade.sh
+++ grade.sh
@@ -1,5 +1,36 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
+
+if [[ -f student-submission/ListExamples.java ]]
+then
+echo "Found the student submission file."
+# grep --color Found the student submission file.
+cd student-submission
+cp ../TestListExamples.java ./
+
+else
+echo "Did not found the student submission file"
+exit 1
+fi
+
+
+javac -cp $CPATH *.java
+if [[ $? -eq 0 ]]
+then 
+echo "compile the files successfully"
+# grep --color compile the files successfully
+
+else
+echo "can not compile the files"
+
+fi
+ 
+
+java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > grade.txt
+grep --color "FAILURES!!!" grade.txt
+
 echo 'Finished cloning'
+cd ..
+java GradeServer 9999 > web.txt
